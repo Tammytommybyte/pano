d4d664dbd4ba5cf50a58ea70299da9600e552bbf
Vladimir Santiago Cortes Hernandez <246395583+Tammytommybyte@users.noreply.github.com>
2026-01-07 10:34:18 +0000

    Se agrega EOF para evitar eel error de COnfig.

--- next.config.cjs
+++ next.config.cjs
@@ -2,14 +2,7 @@
 const nextConfig = {
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
-  },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+EOF
